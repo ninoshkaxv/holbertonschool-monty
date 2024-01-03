a3b78a1814eb8da4220c4a850f97d92af597993f
Ninoshka Perez <perez.ninoshka31@gmail.com>
2024-01-03 14:48:05 -0400

    removing unnecessary functions

--- stack_operations_2.c
+++ stack_operations_2.c
@@ -1,20 +1,10 @@
 #include "monty.h"
 
 /**
- * nop - Does nothing.
+ * f_nop - Does nothing.
  * @stack: A pointer to the stack.
  */
-void nop(stack_t **stack)
+void f_nop(stack_t **stack)
 {
     (void)stack; /* Avoid compiler warning for unused parameter */
 }
-
-/**
- * free_stack - Frees the stack.
- * @stack: A pointer to the stack.
- */
-void free_stack(stack_t **stack)
-{
-    free_stack(stack);
-}
-
