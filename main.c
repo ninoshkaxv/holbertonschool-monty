a26e8372f69a4dd609c56fae830ec2780b0bc292
Ninoshka Perez <perez.ninoshka31@gmail.com>
2024-01-04 09:53:37 -0400

    fix undeclared input

--- main.c
+++ main.c
@@ -1,43 +1,45 @@
-#define _GNU_SOURCE
 #include "monty.h"
 
-unsigned int line_number = 0;
-
 /**
- * main - Entry point of the Monty program.
- * @argc: Number of command-line arguments.
- * @argv: Array of command-line argument strings.
- * Return: EXIT_SUCCESS on success, EXIT_FAILURE on failure.
- */
-int main(int argc, char *argv[])
+*main - Main function
+*@argc: Quantity of arguments that enters the function
+*@argv: Arguments
+*Return: Always Zero
+*/
+
+int main(int argc, char **argv)
 {
-    stack_t *stack = NULL;
-    char *line = NULL;
-    size_t len = 0;
-    size_t read;
-    FILE *file;
+	FILE *fd;
+	unsigned int line_number = 0;
+	char *buff;
+	size_t size = 0;
+	stack_t *stack = NULL;
 
-    if (argc != 2)
-    {
-        fprintf(stderr, "USAGE: monty file\n");
-        exit(EXIT_FAILURE);
-    }
+	if (argc != 2)
+	{
+		dprintf(2, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+	fd = fopen(argv[1], "r+");
 
-    file = fopen(argv[1], "r");
-    if (!file)
-    {
-        fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-        exit(EXIT_FAILURE);
-    }
+	if (!fd)
+	{
+		dprintf(2, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
 
-    while ((read = getline(&line, &len, file)) != -1)
-    {
-        line_number++;
-        parse_and_execute(&stack, line);
-    }
+	while (getline(&buff, &size, fd) != -1)
+	{
+		line_number++;
+		input = _divstring(buff, "\n\t\r ");
+		if (input)
+			get_opcode(input[0], &stack, line_number);
 
-    free_stack(&stack);
-    free(line);
-    fclose(file);
-    exit(EXIT_SUCCESS);
+		free(input);
+	}
+
+	free(buff);
+	free_stack(stack);
+	fclose(fd);
+	return (0);
 }
