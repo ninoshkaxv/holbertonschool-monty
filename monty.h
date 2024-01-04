9879f6dbec9b029a9722b4f0da574325e8dc94a7
Ninoshka Perez <perez.ninoshka31@gmail.com>
2024-01-04 09:50:05 -0400

    Update monty.h with errno.h library

--- monty.h
+++ monty.h
@@ -1,27 +1,61 @@
-#ifndef MONTY_H
-#define MONTY_H
+#ifndef HEADER_H
+#define HEADER_H
 
-#include <stdio.h>
+/* Libraries */
 #include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
 #include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <time.h>
 
-/* Define the structure for the stack */
+/* Global Variables */
+char **input;
+
+/* Structures */
+/**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO
+ */
 typedef struct stack_s
 {
-    int n;
-    struct stack_s *next;
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
 } stack_t;
 
-/* Function prototypes */
-void parse_and_execute(stack_t **stack, char *line);
-void execute_instruction(stack_t **stack, char *instruction, char *argument);
-void f_push(stack_t **stack, char *argument);
-void f_pall(stack_t *stack);
+/**
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct instruction_s
+{
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+
+/* Prototypes */
+void get_opcode(char *buff, stack_t **stack, unsigned int line_number);
+char **_divstring(char *string_pathcpy, char *separator);
+void f_pall(stack_t **stack, unsigned int line_number);
+void f_push(stack_t **stack, unsigned int line_number);
 void f_pint(stack_t **stack, unsigned int line_number);
 void f_pop(stack_t **stack, unsigned int line_number);
 void f_swap(stack_t **stack, unsigned int line_number);
 void f_add(stack_t **stack, unsigned int line_number);
-void f_nop(stack_t **stack);
-void free_stack(stack_t **stack);
-int is_numeric(const char *str);
-#endif /* MONTY_H */
+void f_nop(stack_t **stack, unsigned int line_number);
+void free_stack(stack_t *stack);
+
+#endif
