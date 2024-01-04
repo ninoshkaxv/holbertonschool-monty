0940d0567f2f8b907513cb4d4d52e981785c8c2d
Ninoshka Perez <perez.ninoshka31@gmail.com>
2024-01-04 10:03:13 -0400

    fix betty
    too many lines

--- stack_manipulation.c
+++ stack_manipulation.c
@@ -1,124 +1,101 @@
 #include "monty.h"
-#include <ctype.h>
-
 
 /**
- * f_push - Pushes an element onto the stack.
- * @stack: A pointer to the stack.
- * @argument: The argument to push onto the stack.
+ * f_pall - Prints all the values of the stack
+ * @stack: Stack of nodes
+ * @line_number: Line where the instruction is located
  */
-void f_push(stack_t **stack, char *argument)
+
+void f_pall(stack_t **stack, unsigned int line_number)
 {
-    stack_t *new_node;
 
-    new_node = malloc(sizeof(stack_t));
-    if (!new_node)
-    {
-        fprintf(stderr, "Error: malloc failed\n");
-        exit(EXIT_FAILURE);
-    }
+	(void)(line_number);
 
-    new_node->n = atoi(argument);
-    new_node->next = *stack;
-    *stack = new_node;
+	while (*stack)
+	{
+		printf("%d\n", (*stack)->n);
+		stack = &(*stack)->next;
+	}
 }
 
 /**
- * f_pall - Prints all the values on the stack, starting from the top.
- * @stack: A pointer to the stack.
+ * f_push - Pushes an element at the top of the stack
+ * @stack: Stack of nodes
+ * @line_number: Line where the instruction is located
  */
-void f_pall(stack_t *stack)
+
+void f_push(stack_t **stack, unsigned int line_number)
 {
-    while (stack)
-    {
-        printf("%d\n", stack->n);
-        stack = stack->next;
-    }
+	stack_t *aux = NULL;
+	int integer;
+
+	aux = malloc(sizeof(stack_t));
+	if (aux == NULL)
+	{
+		free(aux);
+		dprintf(2, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
+
+	integer = atoi(input[1]);
+	if (integer == 0)
+	{
+		dprintf(2, "L%i: usage: push integer\n", line_number);
+		free(aux);
+		exit(EXIT_FAILURE);
+	}
+
+	aux->n = integer;
+	aux->prev = NULL;
+	aux->next = *stack;
+
+	if (*stack != NULL)
+		(*stack)->prev = aux;
+	*stack = aux;
 }
 
 /**
- * f_pint - Function to perform pint operation.
- * @stack: Pointer to the stack.
- * @line_number: Line number in the Monty bytecode file.
+ * f_pint - Prints the first node of the stack
+ * @stack: Stack of nodes
+ * @line_number: Line where the instruction is located
  */
+
 void f_pint(stack_t **stack, unsigned int line_number)
 {
-    if (*stack)
-        printf("%d\n", (*stack)->n);
-    else
-    {
-        fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
-        exit(EXIT_FAILURE);
-    }
+	if (!stack || !*stack)
+	{
+		dprintf(2, "L%i: can't pint, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	printf("%d\n", (*stack)->n);
 }
 
 /**
- * free_stack - Frees the stack.
- * @stack: A pointer to the stack.
+ * f_pop- Removes the top element of the stack
+ * @stack: Stack of nodes
+ * @line_number: Line where the instruction is located
  */
-void free_stack(stack_t **stack)
+
+void f_pop(stack_t **stack, unsigned int line_number)
 {
-    stack_t *temp;
+	stack_t *aux = *stack;
 
-    while (*stack)
-    {
-        temp = (*stack)->next;
-        free(*stack);
-        *stack = temp;
-    }
-}
+	if (!*stack || !stack)
+	{
+		dprintf(2, "L%i: can't pop an empty stack\n", line_number);
+		exit(EXIT_FAILURE);
+	}
 
-/**
- * is_numeric - Checks if a string is a numeric value.
- * @str: The string to check.
- * Return: 1 if numeric, 0 otherwise.
- */
-int is_numeric(const char *str)
-{
-    if (str == NULL || *str == '\0')
-        return 0;
-
-    while (*str)
-    {
-        if (!isdigit(*str))
-            return 0;
-        str++;
-    }
-
-    return 1;
-}
-
-/**
- * parse_and_execute - Parse and execute Monty bytecode instructions.
- * @stack: Double pointer to the beginning of the stack.
- * @line: String containing the Monty bytecode instruction.
- */
-void parse_and_execute(stack_t **stack, char *line)
-{
-    unsigned int line_number = 0;
-	char *instruction, *argument;
-    #define DELIMITERS " \t\n"
-
-    instruction = strtok(line, DELIMITERS);
-    if (!instruction)
-        return;
-
-    argument = strtok(NULL, DELIMITERS);
-
-    if (strcmp(instruction, "push") == 0)
-    {
-        if (!argument || !is_numeric(argument))
-        {
-           fprintf(stderr, "L%u: usage: push integer\n", line_number);
-		   exit(EXIT_FAILURE);
-        }
-        f_push(stack, argument);
-    }
-    else if (strcmp(instruction, "pall") == 0)
-    {
-        f_pall(*stack);
-    }
-    // Add more cases for other instructions...
-
-    #undef DELIMITERS
+	if ((*stack)->next != NULL)
+	{
+		*stack = aux->next;
+		(*stack)->prev = NULL;
+		free(aux);
+	}
+	else
+	{
+		free(*stack);
+		*stack = NULL;
+	}
 }
