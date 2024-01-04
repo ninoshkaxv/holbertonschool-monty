665f6b87f19ec35c86e777b60845675779aedec1
Ninoshka Perez <perez.ninoshka31@gmail.com>
2024-01-04 10:22:38 -0400

    mending push and pall errors

--- stack_manipulation.c
+++ stack_manipulation.c
@@ -1,57 +1,52 @@
 #include "monty.h"
 
 /**
- * f_pall - Prints all the values of the stack
- * @stack: Stack of nodes
- * @line_number: Line where the instruction is located
- */
-
-void f_pall(stack_t **stack, unsigned int line_number)
-{
-
-	(void)(line_number);
-
-	while (*stack)
-	{
-		printf("%d\n", (*stack)->n);
-		stack = &(*stack)->next;
-	}
-}
-
-/**
  * f_push - Pushes an element at the top of the stack
  * @stack: Stack of nodes
  * @line_number: Line where the instruction is located
  */
-
 void f_push(stack_t **stack, unsigned int line_number)
 {
-	stack_t *aux = NULL;
-	int integer;
+    stack_t *aux = NULL;
+    int integer;
 
-	aux = malloc(sizeof(stack_t));
-	if (aux == NULL)
-	{
-		free(aux);
-		dprintf(2, "Error: malloc failed\n");
-		exit(EXIT_FAILURE);
-	}
+    if (input[1] == NULL || !is_numeric(input[1]))
+    {
+        dprintf(2, "L%i: usage: push integer\n", line_number);
+        exit(EXIT_FAILURE);
+    }
 
-	integer = atoi(input[1]);
-	if (integer == 0)
-	{
-		dprintf(2, "L%i: usage: push integer\n", line_number);
-		free(aux);
-		exit(EXIT_FAILURE);
-	}
+    aux = malloc(sizeof(stack_t));
+    if (aux == NULL)
+    {
+        dprintf(2, "Error: malloc failed\n");
+        exit(EXIT_FAILURE);
+    }
 
-	aux->n = integer;
-	aux->prev = NULL;
-	aux->next = *stack;
+    integer = atoi(input[1]);
+    aux->n = integer;
+    aux->prev = NULL;
+    aux->next = *stack;
 
-	if (*stack != NULL)
-		(*stack)->prev = aux;
-	*stack = aux;
+    if (*stack != NULL)
+        (*stack)->prev = aux;
+    *stack = aux;
+}
+
+/**
+ * f_pall - Prints all the values of the stack
+ * @stack: Stack of nodes
+ * @line_number: Line where the instruction is located
+ */
+void f_pall(stack_t **stack, unsigned int line_number)
+{
+    (void)(line_number);
+
+    while (*stack)
+    {
+        printf("%d\n", (*stack)->n);
+        stack = &(*stack)->next;
+    }
 }
 
 /**
